8fbfbf1e2e5ade67e8f793d68ed2cd37bd5d4338
DanteNoguez <dantenoguez@gmail.com>
2023-01-20 20:46:56 -0600

    code finished, hyperparameters are not great

--- gpt-wilde.py
+++ gpt-wilde.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
-max_iters = 3000
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'mps'
 eval_iters = 200
-n_embd = 52 # number of embeddings
+n_embd = 384 # number of embeddings
+n_head = 8
+n_layer = 8
+dropout = 0.2
 # ------------
 
 # dataset
@@ -57,21 +60,115 @@ def estimate_loss():
     model.train()
     return out
 
-# super simple bigram model
-class BigramLanguageModel(nn.Module):
+# attention
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        #compute attention scores ("affinities")
+        wei = q @ k.transpose(-2, -1) * C**0.5 # (B,T,C) @ (B,C,T) -> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # B,T,T
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
+        # perform the weighted aggregation of values
+        v = self.value(x) # (B,T,C)
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+# FFN
+class FeedForward(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd), # multiply by 4 like in the paper
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+# LayerNorm
+# Same as batch norm but without buffers, calculating means and std across rows instead of columns, no momentum
+# and no worries about training or test time
+class LayerNorm(nn.Module):
+    def __init__(self, dim, epsilon=1e-5):
+        self.epsilon = epsilon
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+
+    def __call__(self, x):
+        xmean = x.mean(1, keepdim=True) # across rows
+        xvar = x.var(1, keepdim=True)
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+class Block(nn.Module):
+    # transformer block: communication followed by computation"
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffn = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x)) # sum with x is residual connections
+        x = x + self.ffn(self.ln2(x)) # pre-normalization is new, not equal to the paper
+        return x
+
+# super simple transformer (decoder only)
+class Transformer(nn.Module):
 
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        # self.sa_head = Head(n_embd)
+        # self.sa_heads = MultiHeadAttention(6, n_embd//6) # 6 heads 
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        #self.ffn = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size) # language modeling head
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape
 
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        logits = self.lm_head(tok_emb) # (B, T, vocab_size)
+        pos_embd = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_embd # (B,T,C)
+        # x = self.sa_heads(x)
+        x = self.blocks(x) # aply one head of self-attention (B,T,C)
+        # x = self.ffn(x)
+        logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
             loss = None
@@ -86,8 +183,10 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond) 
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
@@ -98,7 +197,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel()
+model = Transformer()
 m = model.to(device)
 
 # create a PyTorch optimizer
